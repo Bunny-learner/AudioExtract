62ff64316402755ba67b79a283b65cfec710ed1e
Ambati Venkata Sai Tejesh <ambatikalyani1582@gmail.com>
2025-06-27 11:05:13 +0530

    Delete backend directory

--- backend/routers/router1.js
+++
@@ -1,7 +0,0 @@
-import  {Router } from 'express'
-import {url,diffurl} from '../controllers/convert.js'
-const router1=Router()
-
-router1.post('/url',url)
-router1.get('/diffurl',diffurl)
-export default router1;
