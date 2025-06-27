b811ae805842ab4f99ab2a1af5f55dad92345b2c
Ambati Venkata Sai Tejesh <ambatikalyani1582@gmail.com>
2025-06-27 12:00:41 +0530

    Update router1.js

--- backend/routers/router1.js
+++ backend/routers/router1.js
@@ -1,8 +1,9 @@
 import  {Router } from 'express'
-import {url,diffurl, videosend} from '../controllers/convert.js'
+import {url,diffurl, videosend,debug} from '../controllers/convert.js'
 const router1=Router()
 
 router1.post('/url',url)
 router1.get('/audio',diffurl)
 router1.get('/video',videosend)
+router1.get('/memory',debug)
 export default router1;
