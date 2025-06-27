166f32d86027cba64e838e44ec7ac9e04bcda32f
Ambati Venkata Sai Tejesh <ambatikalyani1582@gmail.com>
2025-06-27 20:45:58 +0530

    Update start.js

--- backend/start.js
+++ backend/start.js
@@ -1,6 +1,14 @@
 import apk from "./app.js"
 import dotenv from "dotenv"
 dotenv.config()
+import { execSync } from 'child_process';
+
+try {
+  const result = execSync('ls -l ./backend/bin/yt-dlp').toString();
+  console.log('🔍 yt-dlp permissions:\n', result);
+} catch (err) {
+  console.error('❌ Failed to check yt-dlp permissions', err.message);
+}
 
 const PORT = process.env.PORT || 5000;
 try {
